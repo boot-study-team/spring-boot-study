4415b09953f0cf785be600d6a53d90c74d249a58
wangzhiqiang <wangzhiqiang@mobanker.com>
2019-07-08 11:30:09 +0800

    增加feignClient调用

--- spring-rock-parent/spring-rock-application/src/main/java/com/spring/rock/SpringbootApplication.java
+++ spring-rock-parent/spring-rock-application/src/main/java/com/spring/rock/SpringbootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.spring.rock")
 public class SpringbootApplication {
 
     public static void main(String[] args) {
--- spring-rock-parent/spring-rock-consumer/src/main/java/com/spring/rock/controller/ConsumerController.java
+++ spring-rock-parent/spring-rock-consumer/src/main/java/com/spring/rock/controller/ConsumerController.java
@@ -1,6 +1,6 @@
 package com.spring.rock.controller;
 
-import org.springframework.context.annotation.Bean;
+import com.spring.rock.feign.UserFeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +14,9 @@ public class ConsumerController {
     @Resource
     RestTemplate restTemplate;
 
+    @Resource
+    UserFeignClient userFeignClient;
+
     /**
      * restTemplate方式测试调用提供者
      * @return
@@ -22,5 +25,15 @@ public class ConsumerController {
     public String getConsumerInfo(){
         System.out.println(restTemplate.postForObject("http://eureka-1:8081/provider/getInfo",null,String.class));
         return restTemplate.postForEntity("http://eureka-1:8081/provider/getInfo",null,String.class).toString();
+    }
+
+    /**
+     * FeignClient方式测试调用提供者
+     * @return
+     */
+    @RequestMapping("/getConsumerInfoByFeign")
+    public String getConsumerInfoByFeign(){
+        System.out.println(userFeignClient.getInfo());
+        return userFeignClient.getInfo();
     }
 }
