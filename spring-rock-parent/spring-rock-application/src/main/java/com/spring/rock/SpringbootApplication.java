84521a2d7e652212e797fde85c73d354cdde09d9
wzq <12345678>
2020-04-05 20:53:54 +0800

    yml

--- spring-rock-parent/spring-rock-application/src/main/java/com/spring/rock/SpringbootApplication.java
+++ spring-rock-parent/spring-rock-application/src/main/java/com/spring/rock/SpringbootApplication.java
@@ -6,9 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -21,13 +18,13 @@ public class SpringbootApplication {
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
-    @EnableWebSecurity
-    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-        }
-    }
+//    @EnableWebSecurity
+//    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http.csrf().disable();
+//        }
+//    }
 
     @LoadBalanced
     @Bean
